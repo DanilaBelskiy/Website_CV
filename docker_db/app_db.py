ffed13a22d145cdcc3d81fa2aa3983b2aed6b2a7
Danila <belsky32@mail.ru>
2022-05-31 23:03:21 +0300

    Docker containers are connected

--- docker_db/app_db.py
+++ docker_db/app_db.py
@@ -2,9 +2,10 @@ import psycopg2
 
 print('hello world before')
 
-conn = psycopg2.connect(database="stage", user="postgres", password="secret", host="127.0.0.1", port="5460")
+conn = psycopg2.connect(dbname="stage", user="postgres", password="secret", host="db", port="5432")
 cursor = conn.cursor()
 
-cursor.execute("CREATE TABLE hello_world (hello TEXT, world TEXT);")
+cursor.execute("INSERT into test (first_name, second_name) VALUES ('Ivan', 'Ivanov');")
+conn.commit()
 
 print('hello world after')
